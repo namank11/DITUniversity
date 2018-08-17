5f1cd23594d0f30c9e1be18ed27f3ea02bfef4b0
Naman <namank_11@outlook.com>
2018-08-17 09:58:27 +0530

    Initial commit

---
+++ app/src/main/java/com/example/naman/dituniversity/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.naman.dituniversity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView=(WebView)findViewById(R.id.web);
+        WebSettings webSettings=webView.getSettings();
+        webView.loadUrl("https://www.dituniversity.edu.in/");
+        webView.setWebViewClient(new WebViewClient());
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
